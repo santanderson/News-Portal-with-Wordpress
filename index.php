31c67c3ae3e48112ef7fe28d0a171f4239562feb
Anderson Ryan <santoryan1408@gmail.com>
2022-02-25 11:57:56 -0300

    initial structure

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    get_header();
+?>
+
+<section class="urgency"></section>
+
+<section class="hot-news">
+
+</section>
+
+<section class="sports">
+
+</section>
+
+<section class="lifestyle">
+
+</section>
+
+<section class="entertainment">
+
+</section>
+
+
+<?php
+    get_footer();
+?>
